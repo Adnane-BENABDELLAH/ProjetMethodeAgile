b3d24c2448958d3663b8a019dae928c6af73b124
Adnane-BENABDELLAH <adnaneiron9@gmail.com>
2021-04-03 17:27:16 +0100

    sCommit

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Ecommerce Example</title>
+        <title>Laravel Ecommerce</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto:300,400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat%7CRoboto:300,400,700" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Styles -->
@@ -16,97 +17,78 @@
 
     </head>
     <body>
-      <header>
-        <div class="top-nav container">
-          <div class="Logo">Laravel Ecommerce</div>
-          <ul>
-            <li><a href="{{ route('shop.index') }}">Shop</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="{{ route('cart.index') }}">Cart</a></li>
-          </ul>
-        </div> <!-- end top nav -->
+        <div id="app">
+            <header class="with-background">
+                <div class="top-nav container">
+                    <div class="top-nav-left">
+                        <div class="logo">Ecommerce</div>
+                        {{ menu('main', 'partials.menus.main') }}
+                    </div>
+                    <div class="top-nav-right">
+                        @include('partials.menus.main-right')
+                    </div>
+                </div> <!-- end top-nav -->
+                <div class="hero container">
+                    <div class="hero-copy">
+                        <h1>Laravel Ecommerce</h1>
+                        <p>Includes multiple products, categories, a shopping cart and a checkout system with Stripe integration.</p>
+                        <div class="hero-buttons">
+                            <a href="https://trello.com/b/NFrcdfU4/ecommerce-website" class="button button-white">Trello</a>
+                            <a href="https://github.com/Adnane-BENABDELLAH/ProjetMethodeAgile" class="button button-white">GitHub</a>
+                        </div>
+                    </div> <!-- end hero-copy -->
 
-        <div class="hero container">
-          <div class="hero-copy">
+                    <div class="hero-image">
+                        <img src="img/macbook-pro-laravel.png" alt="hero image">
+                    </div> <!-- end hero-image -->
+                </div> <!-- end hero -->
+            </header>
 
-            <h1>Laravel Ecommerce</h1>
-            <p>A practical example of using CSS Grid for a typical website layout.</p>
-          </div> <!-- end hero-copy -->
+            <div class="featured-section">
 
-          <div class="hero-image">
-            <img src="img/macbook-pro-laravel.png" alt="hero image">
-          </div>
-        </div> <!-- end hero -->
-      </header>
+                <div class="container">
+                    <h1 class="text-center">Laravel Ecommerce</h1>
 
-      <div class="featured-section">
-        <div class="container">
-          <h1 class="text-center">CSS Grid Example</h1>
-          <p class="section-description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia provident modi saepe est velit ratione, libero ipsum ullam. Consequatur labore nesciunt cumque, ex molestiae corporis minus obcaecati minima et deserunt! Velit vitae est, dolore molestiae reprehenderit alias porro ab ea?</p>
+                    <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore vitae nisi, consequuntur illum dolores cumque pariatur quis provident deleniti nesciunt officia est reprehenderit sunt aliquid possimus temporibus enim eum hic lorem.</p>
 
-          <div class="text-center button-container">
-            <a href="#" class="button">Featured</a>
-            <a href="#" class="button">On Sale</a>
-          </div>
+                    <div class="text-center button-container">
+                        <a href="#" class="button">Featured</a>
+                        <a href="#" class="button">On Sale</a>
+                    </div>
 
+                    {{-- <div class="tabs">
+                        <div class="tab">
+                            Featured
+                        </div>
+                        <div class="tab">
+                            On Sale
+                        </div>
+                    </div> --}}
 
-          <div class="products text-center">
-            @foreach ($products as $product)
-                <div class="product">
-                  <a href="{{ route('shop.show', $product->slug) }}"><img src="{{asset('img/products/'.$product->slug.'.jpg')}}" alt="product"></a>
-                  <a href="{{ route('shop.show', $product->slug) }}"><span class="product-name">{{ $product->name }}</span></a>
-                  <div class="product-price">{{ $product->presentPrice() }}</div>
-                </div>
-            @endforeach
-          </div> <!-- end products -->
+                    <div class="products text-center">
+                        @foreach ($products as $product)
+                            <div class="product">
+                                <a href="{{ route('shop.show', $product->slug) }}"><img src="{{ productImage($product->image) }}" alt="product"></a>
+                                <a href="{{ route('shop.show', $product->slug) }}"><div class="product-name">{{ $product->name }}</div></a>
+                                <div class="product-price">{{ $product->presentPrice() }}</div>
+                            </div>
+                        @endforeach
 
-          <div class="text-center button-container">
-            <a href="{{ route('shop.index') }}" class="button">View more products</a>
-          </div>
+                    </div> <!-- end products -->
 
-        </div>  <!-- end container -->
+                    <div class="text-center button-container">
+                        <a href="{{ route('shop.index') }}" class="button">View more products</a>
+                    </div>
 
-      </div> <!-- end Featured-section -->
+                </div> <!-- end container -->
 
-      <div class="blog-section">
-        <div class="container">
-          <h1 class="text-center">From Our Blog</h1>
-          <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, officia laborum dolor repellat suscipit impedit consequatur explicabo. Quibusdam, fuga consectetur.</p>
+            </div> <!-- end featured-section -->
 
-          <div class="blog-posts">
-            <div class="blog-post">
-              <a href="#"><img src="img/blog1.png" alt="blog image"></a>
-              <a href="#"><h2 class="blog-title">Blog Post Title 1</h2></a>
-              <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit est blanditiis saepe maiores, quos ipsa laboriosam nihil debitis id distinctio.</div>
-            </div>
+            <blog-posts></blog-posts>
 
-            <div class="blog-post">
-              <a href="#"><img src="img/blog1.png" alt="blog image"></a>
-              <a href="#"><h2 class="blog-title">Blog Post Title 1</h2></a>
-              <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit est blanditiis saepe maiores, quos ipsa laboriosam nihil debitis id distinctio.</div>
-            </div>
+            @include('partials.footer')
 
-            <div class="blog-post">
-              <a href="#"><img src="img/blog1.png" alt="blog image"></a>
-              <a href="#"><h2 class="blog-title">Blog Post Title 1</h2></a>
-              <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit est blanditiis saepe maiores, quos ipsa laboriosam nihil debitis id distinctio.</div>
-            </div>
-          </div> <!-- end blog-posts -->
-        </div> <!-- end container -->
-      </div> <!-- end blog-section -->
-
-      <footer>
-        <div class="footer-content container">
-          <div class="made-with">Made with <i class="fa fa-heart"></i> By Andre Madarang</div>
-          <ul>
-            <li>Follow Me:</li>
-            <li><a href="#"><i class="fa fa-globe"></i></a></li>
-            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-            <li><a href="#"><i class="fa fa-github"></i></a></li>
-            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-          </ul>
-        </div>
-      </footer>
+        </div> <!-- end #app -->
+        <script src="js/app.js"></script>
     </body>
 </html>
